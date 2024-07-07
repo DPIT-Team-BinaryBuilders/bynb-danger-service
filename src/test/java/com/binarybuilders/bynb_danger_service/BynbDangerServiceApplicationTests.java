bd4993e18a249e6b934ac96dc089aa2a9b84e6bf
Radulescu Razvan <proxy.razvan@gmail.com>
2024-07-07 16:52:53 +0300

    add: configuration of the default file tree, connection to the postgres db

---
+++ src/test/java/com/binarybuilders/bynb_danger_service/BynbDangerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binarybuilders.bynb_danger_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BynbDangerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
