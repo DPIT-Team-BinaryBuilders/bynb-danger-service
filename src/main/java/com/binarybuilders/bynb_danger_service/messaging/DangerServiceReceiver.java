a972dabed9a24ee6a57ec9b5a3c517729add39a4
Vlad Sarlea <93834439+Dutu06@users.noreply.github.com>
2024-08-12 18:53:06 +0300

    Created the RabbitMQ Receiver for Adding a New Danger

--- src/main/java/com/binarybuilders/bynb_danger_service/messaging/DangerServiceReceiver.java
+++ src/main/java/com/binarybuilders/bynb_danger_service/messaging/DangerServiceReceiver.java
@@ -1,12 +1,36 @@
 package com.binarybuilders.bynb_danger_service.messaging;
 
+import com.binarybuilders.bynb_danger_service.persistence.DangerEntity;
+import com.binarybuilders.bynb_danger_service.repository.DangerRepository;
+import com.binarybuilders.bynb_danger_service.service.DangerService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DangerServiceReceiver {
+
+    @Autowired
+    private DangerService dangerService;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private DangerRepository dangerRepository;
+
+
     @RabbitListener(queues = RabbitMQConfig.USER_TO_DANGER_QUEUE)
     public void receiveMessage(String message) {
-        System.out.println("Received Message: " + message);
+        try {
+            // Convert the JSON string to a Danger object
+            DangerEntity danger = objectMapper.readValue(message, DangerEntity.class);
+
+            dangerRepository.save(danger);
+            // Process the Danger object
+            System.out.println("Received danger: " + danger);
+            // Handle the danger creation
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/binarybuilders/bynb_danger_service/messaging/DangerServiceSender.java
+++ src/main/java/com/binarybuilders/bynb_danger_service/messaging/DangerServiceSender.java
@@ -1,5 +1,6 @@
 package com.binarybuilders.bynb_danger_service.messaging;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,11 +10,14 @@ public class DangerServiceSender {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
+
+
     public DangerServiceSender(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
     }
 
     public void sendMessage(String message) {
+
         rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, RabbitMQConfig.ROUTING_KEY_DANGER_TO_USER, message);
         System.out.println("Sent message: " + message);
     }
